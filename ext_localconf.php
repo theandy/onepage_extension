0ce497bdab9d555aff6ec7a8f917867b9a95c7ce
andreasloewer <info@andreasloewer.de>
2025-10-10 15:34:27 +0200

    none

---
+++ ext_localconf.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('TYPO3') or die('Access denied.');
+
+/***************
+ * Add default RTE configuration
+ */
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['onepage_extension'] = 'EXT:onepage_extension/Configuration/RTE/Default.yaml';
+
+/***************
+ * PageTS
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:onepage_extension/Configuration/TsConfig/Page/All.tsconfig">');
