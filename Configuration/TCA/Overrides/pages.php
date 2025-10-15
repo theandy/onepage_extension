b2c193e227be61e32221d1d3028cd769d5709456
andreasloewer <info@andreasloewer.de>
2025-10-15 13:18:41 +0200

    none

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -6,12 +6,14 @@ call_user_func(function () {
     $label   = 'OnePage-Section';
     $iconId  = 'onepage-section';
 
+    // Direkt ins TCA schreiben (keine EMU-Helper)
     $GLOBALS['TCA']['pages']['columns']['doktype']['config']['items'][] = [
         $label, $doktype, $iconId,
     ];
 
     $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'][$doktype] = $iconId;
 
+    // Feldausgabe wie Standard-Seite
     $defaultType = \TYPO3\CMS\Core\Domain\Repository\PageRepository::DOKTYPE_DEFAULT;
     if (!empty($GLOBALS['TCA']['pages']['types'][$defaultType])) {
         $GLOBALS['TCA']['pages']['types'][$doktype] = $GLOBALS['TCA']['pages']['types'][$defaultType];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,7 @@
 <?php
 defined('TYPO3') or die('Access denied.');
 
+// Nur TypoScript-Setup/Constants registrieren
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'onepage_extension',
     'Configuration/TypoScript/',
