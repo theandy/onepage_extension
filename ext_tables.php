eaca365740c671df7b25124dab7a7f0e07d868ab
andreasloewer <info@andreasloewer.de>
2025-10-15 13:56:05 +0200

    none

--- ext_tables.php
+++ ext_tables.php
@@ -5,19 +5,26 @@ defined('TYPO3') or die('Access denied.');
 
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 /**
- * Backend-only registrations
+ * Backend registrations
  * - Icon for Doktype 170 (onepage_section)
- *   The Doktype definition itself is done in Configuration/TCA/Overrides/pages.php
+ * - Static TypoScript template (for Include Sets)
  */
 (static function (): void {
+    // Icon
     $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(IconRegistry::class);
-
-    // Page type icon
     $iconRegistry->registerIcon(
-        'apps-pagetree-onepage-section', // icon identifier used in TCA ctrl[typeicon_classes]
+        'apps-pagetree-onepage-section',
         SvgIconProvider::class,
         ['source' => 'EXT:onepage_extension/Resources/Public/Icons/doktype-onepage-section.svg']
+    );
+
+    // Static TypoScript (enables "Include TypoScript sets")
+    ExtensionManagementUtility::addStaticFile(
+        'onepage_extension',
+        'Configuration/TypoScript',
+        'Onepage Extension'
     );
 })();
