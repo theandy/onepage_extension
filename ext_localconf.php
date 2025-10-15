15b6ccef3c28c1667f6b751605e6d95f8ec9427f
andreasloewer <info@andreasloewer.de>
2025-10-15 14:45:50 +0200

    none

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,32 +6,22 @@ defined('TYPO3') or die('Access denied.');
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use AndreasLoewer\OnepageExtension\Controller\OnepageController;
 
-/**
- * RTE preset
- */
+// RTE preset
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['onepage_extension']
     = 'EXT:onepage_extension/Configuration/RTE/Default.yaml';
 
-/**
- * PageTS
- */
+// PageTS
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:onepage_extension/Configuration/TsConfig/Page/All.tsconfig">'
 );
 
-/**
- * Extbase plugin registration: OnepageRenderer
- * - Controller: OnepageController::render
- * - Plugin type: content element
- */
+// Extbase plugin registration (LIST plugin)
 ExtensionUtility::configurePlugin(
-    'OnepageExtension',               // Extension name (Vendor is inferred from composer)
-    'OnepageRenderer',                // Plugin key
+    'OnepageExtension',
+    'OnepageRenderer',
     [
         OnepageController::class => 'render',
     ],
-    [
-        // no non-cacheable actions
-    ],
-    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+    [],
+    ExtensionUtility::PLUGIN_TYPE_PLUGIN
 );
