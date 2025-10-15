b38a80706139aff2893f2b5b6c152ec766a920ac
andreasloewer <info@andreasloewer.de>
2025-10-15 12:23:30 +0200

    none

---
+++ Configuration/Icons.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'onepage-section' => [
+        'provider' => SvgIconProvider::class,
+        'source'   => 'EXT:onepage_extension/Resources/Public/Icons/onepage-section.svg',
+    ],
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,38 +1,12 @@
 <?php
-
 defined('TYPO3') or die('Access denied.');
 
-/**
- * Icon für Doktyp 201: onepage-section
- */
-$iconIdentifier = 'onepage-section';
-$iconSource     = 'EXT:onepage_extension/Resources/Public/Icons/onepage-section.svg';
-
-/** @var \TYPO3\CMS\Core\Imaging\IconRegistry $registry */
-$registry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-    \TYPO3\CMS\Core\Imaging\IconRegistry::class
-);
-
-try {
-    $registry->registerIcon(
-        $iconIdentifier,
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $iconSource]
-    );
-} catch (\Throwable $e) {
-    // Bereits registriert -> ignorieren
-}
-
-/**
- * PageTS laden
- */
+// PageTS laden
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:onepage_extension/Configuration/TsConfig/Page/All.tsconfig">'
 );
 
-/**
- * TypoScript-Setup und -Constants laden
- */
+// TypoScript-Setup und -Constants laden
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'onepage_extension',
     'Configuration/TypoScript/',
