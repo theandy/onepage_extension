e564e66ab05920abd0d80624c824ec22f7c6ae41
andreasloewer <info@andreasloewer.de>
2025-10-15 12:30:33 +0200

    none

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,11 +1,9 @@
 <?php
 declare(strict_types=1);
-
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
-
 return [
     'onepage-section' => [
         'provider' => SvgIconProvider::class,
-        'source'   => 'EXT:onepage_extension/Resources/Public/Icons/onepage-section.svg',
+        'source' => 'EXT:onepage_extension/Resources/Public/Icons/onepage-section.svg',
     ],
 ];
